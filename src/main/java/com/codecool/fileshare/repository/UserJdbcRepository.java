72700a98d0bc2ca4679b2ca84eacc3d0fedb993e
fabadrian <fab.adrian@hotmail.com>
2022-11-30 14:54:30 +0100

    this commit will implement methods of userjdbcrepo

--- src/main/java/com/codecool/fileshare/repository/UserJdbcRepository.java
+++ src/main/java/com/codecool/fileshare/repository/UserJdbcRepository.java
@@ -18,16 +18,64 @@ public class UserJdbcRepository implements UserRepository{
 
     @Override //Roland
     public AppUser findByUsername(String username) {
+        final String sql = "SELECT email, password FROM app_user WHERE email = ?;";
 
+        try(Connection conn = DriverManager.getConnection(DB_URL,USER,PASS)){
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, username);
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+
+            AppUser appUser = new AppUser();
+            appUser.setEmail(resultSet.getString(1));
+            appUser.setPassword(resultSet.getString(2));
+
+            return appUser;
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override //Roland
     public void save(AppUser appUser) throws UserAlreadyExistsException {
+        final String sql = "INSERT INTO app_user(email,password) VALUES(?,?);";
 
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)){
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1,appUser.getEmail());
+            statement.setString(2,appUser.getPassword());
+
+            statement.executeUpdate();
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     @Override //Sziku
     public List<AppUser> getAppUsers() {
+        final String sql = "SELECT email, password FROM app_user;";
+        List<AppUser> results = new ArrayList<>();
 
+        try(Connection conn = DriverManager.getConnection(DB_URL,USER,PASS)){
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            ResultSet resultSet = statement.executeQuery();
+            while(resultSet.next()){
+                AppUser appUser = new AppUser();
+                appUser.setEmail(resultSet.getString(1));
+                appUser.setPassword(resultSet.getString(2));
+
+                results.add(appUser);
+            }
+            return results;
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
